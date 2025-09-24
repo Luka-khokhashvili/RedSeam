44acbecf39ec78cf69ee31f1358496829661e020
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-09-24 18:10:53 +0400

    Fix: cart sidebar layout issue

--- src/components/CartSideBar.tsx
+++ src/components/CartSideBar.tsx
@@ -84,8 +84,9 @@ function CartSideBar({ setShowCartBar }: CartSideBarProps) {
       ></div>
       <div
         className="fixed top-0 right-0 w-[540px] h-screen p-[40px] bg-[#F8F6F7] border border-[#E1DFE1] z-10
-                 grid grid-rows-[auto_62px_minmax(0,1fr)_102px_auto]"
+        grid grid-rows-[auto_62px_minmax(0,1fr)_102px_auto]"
       >
+        {/* Top Bar */}
         <div className="flex w-full justify-between items-center">
           <h2 className="text-[20px] text-[#10151F] font-medium">
             Shopping cart ({products.length})
@@ -100,7 +101,10 @@ function CartSideBar({ setShowCartBar }: CartSideBarProps) {
           </button>
         </div>
 
-        <div className="">
+        <div />
+
+        {/* Main content */}
+        <div className="min-h-0 flex flex-col">
           {products.length <= 0 ? (
             <div className="flex flex-col w-full pt-[150px] justify-center items-center">
               <img src="./CartRed.svg" alt="Cart" />
@@ -118,7 +122,7 @@ function CartSideBar({ setShowCartBar }: CartSideBarProps) {
               </button>
             </div>
           ) : (
-            <div className="min-h-0 flex flex-col">
+            <>
               <div className="flex-1 overflow-y-auto pr-2">
                 <div className="flex flex-col gap-[36px]">
                   {products.map((product, index) => (
@@ -199,18 +203,20 @@ function CartSideBar({ setShowCartBar }: CartSideBarProps) {
                   <span>$ {total}</span>
                 </p>
               </div>
+            </>
+          )}
+        </div>
 
-              <div />
+        <div />
 
-              <div>
-                <button
-                  onClick={() => handleCheckout()}
-                  className="flex w-full justify-center items-center py-[16px] bg-[#FF4000] text-white rounded-[10px] cursor-pointer"
-                >
-                  Go to checkout
-                </button>
-              </div>
-            </div>
+        <div>
+          {products.length > 0 && (
+            <button
+              onClick={() => handleCheckout()}
+              className="flex w-full justify-center items-center py-[16px] bg-[#FF4000] text-white rounded-[10px] cursor-pointer"
+            >
+              Go to checkout
+            </button>
           )}
         </div>
       </div>
