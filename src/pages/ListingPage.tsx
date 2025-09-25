0f80ea44602cea97df3d09a4f23174cb43181799
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-09-25 16:31:58 +0400

    Add filter display badge and page favicon pass

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -74,45 +74,73 @@ function ListingPage() {
   return (
     <section className="flex flex-col px-[100px] mt-[72px]">
       {/* Header */}
-      <div className="w-full flex justify-between items-center">
-        <h1 className="text-[42px] font-semibold">Products</h1>
-        <div className="relative flex items-center gap-[32px]">
-          <p className="text-[12px] text-[#3E424A]">
-            Showing {meta?.from}-{meta?.to} of {meta?.total} results
-          </p>
-          <div className="h-[14px] border border-[#E1DFE1]"></div>
-          <div className="relative" ref={filterRef}>
-            <button
-              onClick={openFilterBox}
-              className="flex items-center text-[16px] gap-[10px] cursor-pointer"
-            >
-              <img className="h-[15px]" src="./Filter.svg" alt="Filters icon" />
-              Filter
-            </button>
-            {filterOpen && <FilterBox setFilters={setFilters} />}
-          </div>
+      <div className="flex w-full flex-col gap-[19px]">
+        <div className="w-full flex justify-between items-center">
+          <h1 className="text-[42px] font-semibold">Products</h1>
+          <div className="relative flex items-center gap-[32px]">
+            <p className="text-[12px] text-[#3E424A]">
+              Showing {meta?.from}-{meta?.to} of {meta?.total} results
+            </p>
+            <div className="h-[14px] border border-[#E1DFE1]"></div>
+            <div className="relative" ref={filterRef}>
+              <button
+                onClick={openFilterBox}
+                className="flex items-center text-[16px] gap-[10px] cursor-pointer"
+              >
+                <img
+                  className="h-[15px]"
+                  src="./Filter.svg"
+                  alt="Filters icon"
+                />
+                Filter
+              </button>
+              {filterOpen && <FilterBox setFilters={setFilters} />}
+            </div>
 
-          <div className="relative" ref={sortRef}>
-            <button
-              onClick={openSort}
-              className="flex items-center text-[16px] gap-[9px] cursor-pointer"
-            >
-              Sort by
-              <img
-                className={`w-[10px] ${
-                  sortOpen && "-rotate-180"
-                } transition duration-100`}
-                src="Arrow.svg"
-                alt="Arrow icon"
-              />
-            </button>
-            {sortOpen && (
-              <SortBox setSort={(sortKey: string) => () => setSort(sortKey)} />
-            )}
+            <div className="relative" ref={sortRef}>
+              <button
+                onClick={openSort}
+                className="flex items-center text-[16px] gap-[9px] cursor-pointer"
+              >
+                Sort by
+                <img
+                  className={`w-[10px] ${
+                    sortOpen && "-rotate-180"
+                  } transition duration-100`}
+                  src="Arrow.svg"
+                  alt="Arrow icon"
+                />
+              </button>
+              {sortOpen && (
+                <SortBox
+                  setSort={(sortKey: string) => () => setSort(sortKey)}
+                />
+              )}
+            </div>
           </div>
         </div>
+        {filters.priceFrom && filters.priceTo && (
+          <div className="flex w-[141px] h-[37px] justify-center items-center border border-[#E1DFE1] rounded-[50px]">
+            <p className="flex gap-[8.62px] text-[14px] text-[#3E424A]">
+              <span>
+                Price: {filters.priceFrom}-{filters.priceTo}
+              </span>
+              <button
+                onClick={() =>
+                  setFilters({ priceFrom: undefined, priceTo: undefined })
+                }
+                className="cursor-pointer"
+              >
+                <img
+                  className="w-[6.75px] aspect-square"
+                  src="/x.svg"
+                  alt="clear"
+                />
+              </button>
+            </p>
+          </div>
+        )}
       </div>
-
       {/* Products Listing */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[24px] gap-y-[48px] mt-[32px]">
         {products.map((product: Product) => (
